8f1e29753b426f628bd68dad4295f2f0bf3508ff
BrianRosenkranz <moracho_22@hotmail.com>
2023-01-22 12:14:33 +0000

    added: style, images, and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let startbtn= document.getElementById("start-button");
+let questionContainer = document.getElementById("question-container")
+
+startbtn.addEventListener("click", quizGame);
+
+function quizGame(){
+startbtn.classList.add("hide")
+}
+function showQuestions(){
+
+}
+function showAnswers(){
+
+}
